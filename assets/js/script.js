63b45ae5c48f4d114a557e9e45dd327e94517898
Ellis Robinson <ellis.robinson@live.co.uk>
2021-07-11 17:17:32 +0000

    create function to record players sequence

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,35 @@
+const gameSequence = [];
+
+var playerSequence = [];
+
 let gameSquare = document.getElementsByClassName("game-square");
 
+let gameSquareNum;
+
+//flashes game squares when clicked
+for (let i = 0; i < gameSquare.length; i++) {
+    gameSquare[i].addEventListener("click", flash)
+}
 
 function startGame() {
 
 }
-//generates an array of 4 random numbers 
+//generates an array of random numbers between 1 and max square length 
 function createSequence() {
-    let sequence = [];
 
-    for ( let i = 0; i <= 4; i++) {
+    playerSequence = [];
 
-        let randomNums = Math.floor(Math.random()*4 + 1)
+    for (let i = 0; i < 5; i++) {
 
-        sequence.push(randomNums);
+        let randomNums = Math.floor(Math.random() * gameSquare.length + 1)
+
+        gameSequence.push(randomNums);
     }
 
-    console.log(sequence);
+    console.log(gameSequence);
 
-        for ( let i = 0; i <= 4; i++) {
-            console.log(document.getElementById(sequence[i]).innerText);
-        }
+    //use function to run animation (flash square) within for loop with a delay
+
 }
 
 function runSequence() {
@@ -36,13 +46,11 @@ function incrementRound() {
 
 
 
-
+//flashes clicked square and creates players sequence
 function flash() {
     $(this).fadeOut(150).fadeIn(150);
-}
-
-
-//flashes game squares when clicked
-for (let i = 0; i < gameSquare.length; i++) {
-    gameSquare[i].addEventListener("click", flash)
+    gameSquareNum = parseInt(this.innerHTML)
+    playerSequence.push(gameSquareNum);
+    console.log(playerSequence);
+    console.log(gameSquareNum);
 }
