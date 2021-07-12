b9c80e060d371c9eff4ec216459cd37449ddf6a3
Ellis Robinson <ellis.robinson@live.co.uk>
2021-07-12 16:36:06 +0000

    created functions: createSequence, flashSquare, createPlayerSequence, checkSequence

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,31 @@
-const gameSequence = [];
+let gameSequence = [];
 
-var playerSequence = [];
+let playerSequence = [];
 
-let gameSquare = document.getElementsByClassName("game-square");
+const gameSquare = document.getElementsByClassName("game-square");
 
-let gameSquareNum;
-
-//flashes game squares when clicked
-for (let i = 0; i < gameSquare.length; i++) {
-    gameSquare[i].addEventListener("click", flash)
-}
-
+// starts game
 function startGame() {
-
+    createSequence();
+    flashSequence();
 }
-//generates an array of random numbers between 1 and max square length 
+
+//targets game squares and flashes them in order of game sequence
+function flashSequence() {
+    
+    for (let i = 0; i < gameSequence.length; i++) {
+        setTimeout(function(){
+            flashSquare(gameSequence[i]-1)
+        }, 1000*(i+1));
+      }
+    
+}
+
+//clears player and game sequence. generates an array of random numbers between 1 and max square length 
 function createSequence() {
 
     playerSequence = [];
+    gameSequence = [];
 
     for (let i = 0; i < 5; i++) {
 
@@ -28,29 +36,49 @@ function createSequence() {
 
     console.log(gameSequence);
 
-    //use function to run animation (flash square) within for loop with a delay
-
 }
 
-function runSequence() {
-
+//flashes selected square
+function flashSquare(i) {
+        gameSquare[i].className = "game-square red";
+        setTimeout(function(){
+           gameSquare[i].className = "game-square blue";
+        }, 150);
 }
 
-function checkSequence() {
-
-}
-
-function incrementRound() {
-
-}
-
-
-
-//flashes clicked square and creates players sequence
-function flash() {
-    $(this).fadeOut(150).fadeIn(150);
-    gameSquareNum = parseInt(this.innerHTML)
-    playerSequence.push(gameSquareNum);
+//adds the integer of selected game square to player sequence array
+function createPlayerSequence(i) {
+    let squareNumber = +gameSquare[i].dataset.square
+    playerSequence.push(squareNumber);
     console.log(playerSequence);
-    console.log(gameSquareNum);
+}
+
+//listening for if start button is clicked
+document.querySelector("#start-btn").addEventListener("click", startGame);
+
+//listening for if game square is clicked
+for(let i = 0; i < gameSquare.length; i++){
+    gameSquare[i].addEventListener("click", function() {
+        flashSquare(i);
+        createPlayerSequence(i);
+        checkSequences();
+        });
+}
+
+//if player sequence length is the same as game sequence length compare both, then clear player sequence
+function checkSequences() {
+    if (gameSequence.length === 0) {
+        console.log("press start");
+    }else if (playerSequence.length === gameSequence.length) {
+        console.log("sequences same length");
+        if (playerSequence.toString() === gameSequence.toString()) {
+            console.log("correct!");
+            playerSequence = [];
+        } else {
+            console.log("incorrect!");
+            playerSequence = [];
+        }
+    } else if (playerSequence.length < gameSequence.length) {
+        console.log("click more squares");
+    }
 }
